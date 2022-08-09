89de4dd7559bab4a41790be3c074767dffb7edbe
yongsin100 <yongsin100@nate.com>
2022-08-09 23:48:33 +0900

    firebase push

--- app/src/main/java/com/weplayNew/MainActivity.java
+++ app/src/main/java/com/weplayNew/MainActivity.java
@@ -4,31 +4,38 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
+
 import android.content.Intent;
-import android.net.Uri;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-import java.time.Clock;
+import java.net.URISyntaxException;
 
+
+/**
+ * MainActivity
+ */
 public class MainActivity extends AppCompatActivity {
 
     private WebView web;
     private String url = "http://weplay.pe.kr/";
 
+    /**
+     * onCreate
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
@@ -40,18 +47,14 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<String> task) {
                         if (!task.isSuccessful()) {
-                            //System.out.println("========== Fetching FCM registration token failed ==========" + task.getException());
                             return;
                         }
-
-                        // Get new FCM registration token
-                        String token = task.getResult();
-
-                        //Toast.makeText(MainActivity.this, "" + , Toast.LENGTH_SHORT).show();
                         FirebaseMessaging.getInstance().subscribeToTopic("ALL");
+                        // Get new FCM registration token
+                        /*String token = task.getResult();
+                        Toast.makeText(MainActivity.this, "" + , Toast.LENGTH_SHORT).show();*/
                     }
                 });
-
 
         web = (WebView) findViewById(R.id.webView);
         /* 웹 세팅 작업하기 */
@@ -61,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setJavaScriptCanOpenWindowsAutomatically(false);
         webSettings.setAllowFileAccessFromFileURLs(true);
         webSettings.setAllowUniversalAccessFromFileURLs(true);
-        webSettings.setSupportMultipleWindows(false);//true로 하면 라이브리 로그인 안됨.
+        webSettings.setSupportMultipleWindows(false); //true로 하면 라이브리 로그인 안됨. kakao = true ??
         webSettings.setUseWideViewPort(true);  //html 컨텐츠가 웹뷰에 맞게 나타남
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setSaveFormData(true);
@@ -70,15 +73,17 @@ public class MainActivity extends AppCompatActivity {
             webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         }
 
-        // Keep the splash screen visible for this Activity
-        //splashScreen.setKeepOnScreenCondition(() -> true );
-        //startSomeNextActivity();
-
-        web.setWebViewClient(new WebViewClient());
+        web.setWebViewClient(new CustomsWebViewClient());
         web.setWebChromeClient(new WebChromeClient());
         web.loadUrl(url);
     }
 
+    /**
+     * onKeyDown
+     * @param keyCode
+     * @param event
+     * @return
+     */
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
@@ -88,6 +93,53 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * CustomsWebViewClient
+     */
+    public class CustomsWebViewClient extends WebViewClient {
+        private static final String TAG = "CustomsWebViewClient";
+
+        /**
+         * shouldOverrideUrlLoading
+         * @param view
+         * @param request
+         * @return
+         */
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            Log.d(TAG, " ========== shouldOverrideUrlLoading ========== ");
+
+            if (request.getUrl().getScheme().equals("intent")) {
+                try {
+                    // Intent 생성
+                    Intent intent = Intent.parseUri(request.getUrl().toString(), Intent.URI_INTENT_SCHEME);
+                    PackageManager packageManager = getPackageManager();
+
+                    // 실행 가능한 앱이 있으면 앱 실행
+                    if (intent.resolveActivity(packageManager) != null) {
+                        startActivity(intent);
+                        return true;
+                    }else if(request.getUrl().toString().startsWith("intent:kakao") ||
+                            request.getUrl().toString().startsWith("intent://plusfriend/talk")){
+                        view.loadUrl("https://play.google.com/store/apps/details?id=com.kakao.talk");
+                    }
+
+                    // Fallback URL이 있으면 현재 웹뷰에 로딩
+                   String fallbackUrl = intent.getStringExtra("browser_fallback_url");
+                    if (fallbackUrl != null) {
+                        view.loadUrl(fallbackUrl);
+                        return true;
+                    }
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "Invalid intent request");
+                }
+            }
+            //true는 호스트가 제어를 하고 처리했다를 반환, false을 반환하여 WebView가 평소와 같이 URL로드를 진행하도록 처리
+            return false;
+        }
     }
 
 }
